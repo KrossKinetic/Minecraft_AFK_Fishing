5a9f2be764f93ecd29f69f093dcc7056df275513
krosskinetic <anshumaanvikramsinghlucknow@gmail.com>
2023-09-02 15:09:59 -0400

    Adding introduction message

--- afkfishing.py
+++ afkfishing.py
@@ -14,9 +14,6 @@ def on_press(key):
             while True:
                 #print("color got: " + str(y)w da )
                 x = pyautogui.pixelMatchesColor(1722, 841, y,tolerance=40)
-                '''z = pyautogui.pixel(1722,841)
-                print("color need: " + str(z))
-                print(x)'''
                 if x == True:
                     #y = pyautogui.pixel(1722,841) 
                     mouse.click(Button.right)
@@ -24,6 +21,11 @@ def on_press(key):
     except AttributeError:
         print('special key {0} pressed'.format(
             key))
+
+print("Hey! This is an afk fishing program.")
+print("To start the program, press run and then open up minecraft.\n After opening the world and standing at the fishing spot ( look at the video ) Simply press the left alt key to start the program.")
+print("To close the program, simply kill the code.")
+
 
 with keyboard.Listener(
         on_press=on_press) as listener:

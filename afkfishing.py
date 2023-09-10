1e56246e8000a938f385afde805abf4e6f396c88
krosskinetic <anshumaanvikramsinghlucknow@gmail.com>
2023-09-09 22:46:48 -0400

    testing values

--- afkfishing.py
+++ afkfishing.py
@@ -4,14 +4,15 @@ import pyautogui
 import time
 
 mouse = Controller()
-    
+
 def on_press(key):
+    print(key)
     try:
         if key == keyboard.Key.alt_l:
-            y = pyautogui.pixel(1722*1.33,841*1.9)
+            y = pyautogui.pixel(2239,1245)
             print("Fishing Rod found. Starting to fish.")
             while True:
-                x = pyautogui.pixelMatchesColor(1722*1.33, 841*1.9, y,tolerance=40)
+                x = pyautogui.pixelMatchesColor(2239, 1245, y,tolerance=40)
                 if x == True:
                     mouse.click(Button.right)
                     time.sleep(2)
@@ -23,9 +24,6 @@ print("Hey! This is an afk fishing program.")
 print("To start the program, press run and then open up minecraft.\n After opening the world and standing at the fishing spot ( look at the video ) Simply press the left alt key to start the program.")
 print("To close the program, simply kill the code.")
 #display=input("Enter the display resolution seperated by x: ").split('x')
-while True:
-    pyautogui.position
-
 
 with keyboard.Listener(
         on_press=on_press) as listener:
